45b6560d0dffeda431f63fdf5a0e4837b103b086
Rohitash Chandra <c.rohitash@gmail.com>
2018-09-27 17:19:58 +1000

    Revert "Update langevindynamics_fnn.py"

--- langevindynamics_fnn.py
+++ langevindynamics_fnn.py
@@ -119,8 +119,8 @@ class Network:
         fx = np.zeros(size)
 
         for i in xrange(0, depth):
-            for j in xrange(0, size):
-                pat = j
+            for i in xrange(0, size):
+                pat = i
                 Input = data[pat, 0:self.Top[0]]
                 Desired = data[pat, self.Top[0]:]
                 self.ForwardPass(Input)
